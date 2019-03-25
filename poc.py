3b664ee086d06b361b91ec250cd6aa61da0cc4cb
gossamr <git@gossamr.org>
2019-03-25 19:42:42 +0800

    added broadcast hash of signed message; smarter encoding

--- poc.py
+++ poc.py
@@ -21,13 +21,13 @@ def build_preimage():
 
 def build_b56t(preimage):
     """Builds a base56 encoded SHA256 image from preimage."""
-    hex_image = sha256(preimage.encode('utf-8')).hexdigest()
+    hex_image = sha256(preimage).hexdigest()
     b56_image = base56.encode(base16.decode(hex_image.upper()))
     return b56_image[:12] # is this safe?
 
 def verify_signed_b56t(preimage, base56_pubkey, signed_message):
     """Takes a preimage and verifies it's what was signed by a given pubkey"""
-    b56t = build_b56t(preimage)
+    b56t = build_b56t(preimage.encode('utf-8'))
     BTCmsg = BitcoinMessage(b56t)
     address = P2PKHBitcoinAddress(base56_pubkey)
     
@@ -42,7 +42,7 @@ def verify_signed_b56t(preimage, base56_pubkey, signed_message):
 # pimg = build_preimage()
 # For testing purposes
 pimg = "530784,0000000000000000000b97ababc250fcc1125fe8fa45aea5ad094fa645816210,2018-07-06T22:53:00.642619"
-b56t = build_b56t(pimg)
+b56t = build_b56t(pimg.encode('utf-8'))
 print("Here is the preimage we store in our private DB: " + pimg)
 print("Here is the truncated base56 encoded SHA256 hash sent to prover: " + b56t + '\n')
 
@@ -50,10 +50,11 @@ demo_key = CBitcoinSecret('KyEBGff4uJcp5mCyuBhBm6KmbDnyBe5mmdjT7dueSaE4a71yHYDa'
 demo_pubkey = P2PKHBitcoinAddress.from_pubkey(demo_key.pub)
 
 signed_message = SignMessage(demo_key, BitcoinMessage(b56t))
-# if we wanted to prove when we got signed_message without revealing signed_message, we run it through *build_b56t* and broadcast that hash.
 
 print ("Bitcoin public Key: " + str(demo_pubkey))
 print ("Message signed with Bitcoin Key by prover: " + str(signed_message) + '\n')
+# if we wanted to prove when we got signed_message without revealing signed_message, we run it through *build_b56t* and broadcast that hash.
+print ("Can broadcast this truncated base56 encoded SHA256 hash of signed message: " + build_b56t(signed_message) + '\n')
 
 verify_signed_b56t(pimg, str(demo_pubkey), signed_message)
 
