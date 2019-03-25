49980b37ba932c5fd9074e0ed73eed0b20c967ca
gossamr <git@gossamr.org>
2019-03-25 19:23:01 +0800

    updated for python 3.7.1

--- poc.py
+++ poc.py
@@ -21,7 +21,7 @@ def build_preimage():
 
 def build_b56t(preimage):
     """Builds a base56 encoded SHA256 image from preimage."""
-    hex_image = sha256(preimage).hexdigest()
+    hex_image = sha256(preimage.encode('utf-8')).hexdigest()
     b56_image = base56.encode(base16.decode(hex_image.upper()))
     return b56_image[:12] # is this safe?
 
@@ -32,7 +32,7 @@ def verify_signed_b56t(preimage, base56_pubkey, signed_message):
     address = P2PKHBitcoinAddress(base56_pubkey)
     
     vm = VerifyMessage(address, BTCmsg, signed_message)
-    print(address.__str__() + u" signed " + BTCmsg.__str__() + u"? " + unicode(vm))
+    print(address.__str__() + u" signed " + BTCmsg.__str__() + u"? " + str(vm))
     print(preimage + " hashes to " + b56t)  
     return vm 
 
@@ -43,8 +43,8 @@ def verify_signed_b56t(preimage, base56_pubkey, signed_message):
 # For testing purposes
 pimg = "530784,0000000000000000000b97ababc250fcc1125fe8fa45aea5ad094fa645816210,2018-07-06T22:53:00.642619"
 b56t = build_b56t(pimg)
-print "Here is the preimage we store in our private DB: " + pimg
-print "Here is the truncated base56 encoded SHA256 hash sent to prover: " + b56t + '\n'
+print("Here is the preimage we store in our private DB: " + pimg)
+print("Here is the truncated base56 encoded SHA256 hash sent to prover: " + b56t + '\n')
 
 demo_key = CBitcoinSecret('KyEBGff4uJcp5mCyuBhBm6KmbDnyBe5mmdjT7dueSaE4a71yHYDa')
 demo_pubkey = P2PKHBitcoinAddress.from_pubkey(demo_key.pub)
@@ -52,8 +52,8 @@ demo_pubkey = P2PKHBitcoinAddress.from_pubkey(demo_key.pub)
 signed_message = SignMessage(demo_key, BitcoinMessage(b56t))
 # if we wanted to prove when we got signed_message without revealing signed_message, we run it through *build_b56t* and broadcast that hash.
 
-print "Bitcoin public Key: " + str(demo_pubkey.__str__())
-print "Message signed with Bitcoin Key by prover: " + signed_message + '\n'
+print ("Bitcoin public Key: " + str(demo_pubkey))
+print ("Message signed with Bitcoin Key by prover: " + str(signed_message) + '\n')
 
-verify_signed_b56t(pimg, str(demo_pubkey.__str__()), signed_message)
+verify_signed_b56t(pimg, str(demo_pubkey), signed_message)
 
